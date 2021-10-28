507a37961c371e9df89cb77af2f6a0aa865e154f
yitzy32 <yitzybratt32@gmail.com>
2021-10-28 19:28:07 -0400

    add frontend files and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
+    root :to => 'home#index'
+
     get "/states" => "state_datum#index"
     get "/states/:id" => "state_datum#show"
   end

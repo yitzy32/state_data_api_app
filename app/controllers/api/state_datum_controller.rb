97dbf730c47f2527aa1575a14821e1575352ba0b
yitzy32 <yitzybratt32@gmail.com>
2021-10-27 22:35:13 -0400

    state show uncomment cors file

--- app/controllers/api/state_datum_controller.rb
+++ app/controllers/api/state_datum_controller.rb
@@ -3,4 +3,9 @@ class Api::StateDatumController < ApplicationController
         @states = StateDatum.all
         render "index.json.jb"
     end
+
+    def show
+        @state = StateDatum.find_by(id: params[:id])
+        render "show.json.jb"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/states" => "state_datum#index"
+    get "/states/:id" => "state_datum#show"
   end
 end

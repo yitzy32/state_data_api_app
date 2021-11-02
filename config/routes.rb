50b4af2400e7ce26f3540cba8c04ff85431c6a55
yitzy32 <yitzybratt32@gmail.com>
2021-11-01 22:55:12 -0400

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
+  # root :to => 'home#index'
+  get "/" => "home#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  root :to => 'home#index'
   namespace :api do
 
     get "/states" => "state_datum#index"
